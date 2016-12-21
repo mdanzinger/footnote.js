7e088b45c4fc97d27851905d15042d58e34143ef
Mendy Danziner <mdanzinger@ghostdev.ca>
2016-12-21 17:20:01 -0500

    Add getFootnoteElements function

--- footnote.js
+++ footnote.js
@@ -1,18 +1,48 @@
 var footnote = (function(){
     "use strict";
-    function init() {
-       //Core footnote logic
-    };
 
-    // Close function
+    var _allFootnoteElements = [];   // An array of all footnote elements.
 
-
+    // Default Settings/Configurations
     var config = {
         clickAnywhereToClose: true,
-
+        footnotes: _allFootnoteElements,
     };
+
+
+    function init() {
+        _getFootnoteElements();
+    }
+
+
+
+    /**
+     *  Collects all HTML elements that have footnotes
+     *
+     * @returns {{Array}}
+     * @private
+     */
+    function _getFootnoteElements(){
+        let elements = document.querySelectorAll("*[footnote]");
+
+        // If footnote elements exist and they're not manually configured...
+        if (elements.length > 0 && _allFootnoteElements.length === 0) {
+            for (var i = 0, elmsLength = elements.length; i < elmsLength; i++) {
+                var currentElement = elements[i];
+                _allFootnoteElements.push({
+                    element: currentElement,
+                    footnote: currentElement.getAttribute('footnote'),
+                });
+            }
+        }
+    }
+
+
+
     return {
         init:init,
+        disable:disableFootnotes,
         config:config
     }
+
 })();
