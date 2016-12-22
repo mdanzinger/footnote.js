8b65771bda33ad955601403216baa70231507103
Mendy Danziner <mdanzinger@ghostdev.ca>
2016-12-22 16:02:13 -0500

    Add some helper functions

--- footnote.js
+++ footnote.js
@@ -5,22 +5,27 @@ var footnote = (function(){
 
     // Default Settings/Configurations
     var config = {
-        clickAnywhereToClose: true,
-        footnotes: _allFootnoteElements,
+        clickAnywhereToClose:       true,
+        hover:                      false,
+        footnotes:                  _allFootnoteElements,
     };
 
 
+    /**
+     * Initialization function - this starts the footnote plugin.
+     *  @public
+     */
     function init() {
         _getFootnoteElements();
+        _addFootnoteEvents();
+        footnoteBox.render();
     }
 
 
 
     /**
      *  Collects all HTML elements that have footnotes
-     *
-     * @returns {{Array}}
-     * @private
+     *  @private
      */
     function _getFootnoteElements(){
         let elements = document.querySelectorAll("*[footnote]");
@@ -37,11 +42,70 @@ var footnote = (function(){
         }
     }
 
+    /**
+     *  Creates an event for all footnote elements (Either hover, or click, as set in config)
+     * @private
+     */
+    function _addFootnoteEvents(){
+        if(_allFootnoteElements.length > 0) {
+            _allFootnoteElements.forEach(function(footnote){
+                    footnote.element.addEventListener((config.hover ? "mouseover" : "click"), footnoteBox.place(footnote), false);
+            })
+        }
+    }
+
+
+    /**
+     * Footnote object - all methods pertaining to the actual footnote box.
+     * @type {{render: footnoteBox.render, place: footnoteBox.place, destroy: footnoteBox.destroy}}
+     */
+   var footnoteBox = {
+       render: function(){
+           // Create box
+           this.container = document.createElement("div");
+           this.box       = document.createElement("div");
+           this.note      = document.createElement("div");
+           this.close     = document.createElement("span");
+
+           this.container.className = "footnote-container";
+           this.box.className       = "footnote";
+           this.note.className      = "note";
+           this.close.className     = "close";
+
+           this.container.appendChild(this.box);
+           this.box.appendChild(this.note);
+           this.box.appendChild(this.close);
+           document.body.appendChild(this.container);
+       },
+       place: function(targetFootnote){
+           return function(){
+               // Get footnote position, place box by the position
+               alert("clicked");
+          }
+       },
+       destroy: function() {
+           this.container.parentNode.removeChild(this.container);
+       }
+
+    };
+
+
+    /**
+     * Removes all event listeners for all footnotes
+     */
+    function destroy(){
+        footnoteBox.destroy();
+        //TODO Fix removeEventListener...
+        _allFootnoteElements.forEach(function(footnote) {
+            footnote.element.removeEventListener((config.hover ? "hover" : "click"), footnoteBox.place(footnote), false);
+        })
+    }
+
 
 
     return {
         init:init,
-        disable:disableFootnotes,
+        destroy:destroy,
         config:config
     }
 
